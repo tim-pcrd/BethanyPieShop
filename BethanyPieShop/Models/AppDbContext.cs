75b8d1d1ea8aeedec103d7e8262963a56f4fb82c
iamtimp <timp@tuta.io>
2019-04-24 14:45:22 +0200

    First commit

---
+++ BethanyPieShop/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BethanyPieShop.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Pie> Pies { get; set; }
+    }
+}
---
+++ BethanyPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanyPieShop.Models;
+
+namespace BethanyPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
